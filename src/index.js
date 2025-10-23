5edd3e2e87ebe3019d645968e73c2f7a2c554f4d
maciek <mrobacki@gmail.com>
2025-10-23 13:15:03 +0200

    stars component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+
+import StarRating from "./components/ui/StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={10}
+      color="red"
+      size="20"
+      className="stars"
+      defaultRating={3}
+    />
   </React.StrictMode>
 );
