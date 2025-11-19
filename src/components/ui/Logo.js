31b2fd04b5354a3a8c817481c53924b16b95fae5
maciek <mrobacki@gmail.com>
2025-11-19 11:40:36 +0100

    logo

--- src/components/ui/Logo.js
+++ src/components/ui/Logo.js
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
     <div className="logo">
-      <span role="img">🍿</span>
+      <span role="img">🎬</span>
       <h1>React Movie App</h1>
     </div>
   );
